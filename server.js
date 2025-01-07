b73c0ff5a3bc3b037adc468143b8c99cf4ce2cba
aconstanza16 <anthonyconstanza1@gmail.com>
2025-01-07 16:04:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-const express = require('express'); // Simplified example
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
